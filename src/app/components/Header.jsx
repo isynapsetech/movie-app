776f6ec977e6880497daca05402970506288884b
Indranil <isynapsetech@gmail.com>
2025-09-07 18:10:57 +0100

    Add Header component to RootLayout for improved site structure

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+        <ul className="flex gap-4">
+            <li><Link href="/sign-in">Sign In</Link></li>
+            <li className="hidden sm:block"><Link href="/">Home</Link></li>
+            <li className="hidden sm:block"><Link href="/about">About</Link></li>
+        </ul>
+        <Link href="/">
+            <span className="text-2xl font-bold bg-amber-400 text-black px-2 py-1 rounded-md mr-2">IMDB</span>
+            <span className="text-xl font-bold">Clone</span>
+        </Link>
+    </div>
+  );
+}
